22c2462c45c2709d191b73687852b8fb9fa63bbe
SanniMuhammed <young.muhammedd@gmail.com>
2026-08-27 19:46:24 +0100

    fix: remove unused navigation imports

--- lib/features/stock/presentation/screens/add_edit_product_screen.dart
+++ lib/features/stock/presentation/screens/add_edit_product_screen.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../../../app/providers.dart';
 import '../../../../core/errors/failure.dart';
