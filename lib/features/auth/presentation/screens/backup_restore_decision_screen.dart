fbccb0158fb066fadf4067ae4d95a94c5d1de5b5
SanniMuhammed <young.muhammedd@gmail.com>
2026-09-06 09:04:05 +0100

    fix: improve restore flow v2

--- lib/features/auth/presentation/screens/backup_restore_decision_screen.dart
+++ lib/features/auth/presentation/screens/backup_restore_decision_screen.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:path/path.dart' as p;
 
 import '../../../../app/providers.dart';
