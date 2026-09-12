5c6425fc1679f1feff7782240595459697b31510
Sanni <60125734+SanniMuhammed@users.noreply.github.com>
2026-09-12 09:39:18 +0100

    Use package import for Supabase config

--- lib/features/more/settings/presentation/screens/fulus_cloud_connection_screen.dart
+++ lib/features/more/settings/presentation/screens/fulus_cloud_connection_screen.dart
@@ -6,7 +6,7 @@ import 'package:package_info_plus/package_info_plus.dart';
 import 'package:ulid/ulid.dart';
 
 import '../../../../../app/providers.dart';
-import '../../../../../core/config/supabase_config.dart';
+import 'package:fulus_mobile/core/config/supabase_config.dart';
 import '../../../../../core/errors/failure.dart';
 import '../../../../../shared/widgets/widgets.dart';
 
