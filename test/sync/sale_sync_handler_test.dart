32d9e189d71342a29439abe15c2004e74fb2bc10
Sanni <60125734+SanniMuhammed@users.noreply.github.com>
2026-09-15 09:05:30 +0100

    fix: import SyncStatus in sale sync test

--- test/sync/sale_sync_handler_test.dart
+++ test/sync/sale_sync_handler_test.dart
@@ -1,4 +1,5 @@
 import 'package:fulus_mobile/data/local/database/database.dart';
+import 'package:fulus_mobile/data/local/database/tables.dart';
 import 'package:fulus_mobile/data/remote/endpoints/sales_api.dart';
 import 'package:fulus_mobile/data/repositories/customer_credit_repository_impl.dart';
 import 'package:fulus_mobile/data/repositories/sale_repository_impl.dart';
