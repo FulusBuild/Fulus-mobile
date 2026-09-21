7b6f7b52f8abe6915c237f124b3046bb7ba59790
FulusBuild <sannimetaisland@gmail.com>
2026-09-21 15:20:51 +0100

    fix(sync): import Drift companions for conflict resolution

--- lib/sync/sync_conflict_resolver.dart
+++ lib/sync/sync_conflict_resolver.dart
@@ -1,3 +1,5 @@
+import 'package:drift/drift.dart';
+
 import '../data/local/database/database.dart';
 import '../data/remote/fulus_canonical_reconciler_typed.dart';
 import '../data/remote/fulus_connection_state.dart';
