22215abdfd58bbd198759e68079a43818160cfd1
FulusBuild <sannimetaisland@gmail.com>
2026-09-21 15:29:48 +0100

    fix(test): import Drift values and device registration type

--- test/sync/sync_conflict_resolver_test.dart
+++ test/sync/sync_conflict_resolver_test.dart
@@ -1,3 +1,4 @@
+import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -5,6 +6,7 @@ import 'package:mocktail/mocktail.dart';
 import 'package:fulus_mobile/data/local/database/database.dart';
 import 'package:fulus_mobile/data/remote/fulus_canonical_reconciler_typed.dart';
 import 'package:fulus_mobile/data/remote/fulus_connection_state.dart';
+import 'package:fulus_mobile/data/remote/fulus_device_registration.dart';
 import 'package:fulus_mobile/data/remote/fulus_sync_api.dart';
 import 'package:fulus_mobile/data/repositories/customer_repository_impl.dart';
 import 'package:fulus_mobile/domain/entities/customer.dart';
