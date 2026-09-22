432b597889d1d912e31f9a34fb1fe88f82a9a6b2
FulusBuild <sannimetaisland@gmail.com>
2026-09-22 07:44:49 +0100

    fix(sync): remove duplicate stock adjustment mapping

--- lib/data/remote/fulus_sync_api.dart
+++ lib/data/remote/fulus_sync_api.dart
@@ -142,7 +142,7 @@ class FulusSyncApi implements FulusCanonicalEntityFetcher, FulusCanonicalBatchEn
             'expense.create' => 'expense_create',
             'return.create' => 'return_create',
             'stock_movement.create' => 'inventory_adjust',
-            'stock_adjustment.create' => 'inventory_set',\n            'stock_adjustment.create' => 'inventory_set',
+            'stock_adjustment.create' => 'inventory_set',
             'location.create' => 'location_create',
             'income.create' => 'income_create',
             'cash_drawer_shift.create' => 'cash_drawer_open',
