1a6ce866de1e408a625271a45324019d3157568a
FulusBuild <sannimetaisland@gmail.com>
2026-09-23 10:04:09 +0100

    fix(sync): sanitize cloud error messages in auth flows

--- lib/features/auth/presentation/screens/cloud_restore_screen.dart
+++ lib/features/auth/presentation/screens/cloud_restore_screen.dart
@@ -13,6 +13,7 @@ import '../../../../data/remote/cloud_restore_coordinator.dart';
 import '../../../../data/remote/endpoints/cloud_restore_api.dart';
 import '../../../../domain/entities/business_settings.dart';
 import '../../../../shared/widgets/widgets.dart';
+import '../../../../../sync/sync_user_message.dart';
 
 /// Restores a Fulus installation from the user's single Fulus Cloud business.
 ///
@@ -207,7 +208,7 @@ class _CloudRestoreScreenState extends ConsumerState<CloudRestoreScreen> {
       if (mounted) {
         setState(() {
           _busy = false;
-          _error = error.toString().replaceFirst('Bad state: ', '');
+          _error = syncUserMessage(error);
           _status = 'Ready to restore your business.';
         });
       }
