cf535be41ceb08bbcd314a0c9862d3f52fe1c9fb
FulusBuild <sannimetaisland@gmail.com>
2026-09-23 22:42:57 +0100

    fix: import Dio exception type in refresh tests

--- test/data/remote/api_client_test.dart
+++ test/data/remote/api_client_test.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
