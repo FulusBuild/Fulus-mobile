6f30081679cfa4ca1642aed5c9e5e89cd974f9b5
FulusBuild <sannimetaisland@gmail.com>
2026-09-24 03:14:02 +0100

    test: import Drift Value in credit reconciliation coverage

--- test/repository/customer_credit_repository_test.dart
+++ test/repository/customer_credit_repository_test.dart
@@ -1,3 +1,4 @@
+import 'package:drift/drift.dart';
 import 'package:fulus_mobile/data/local/database/database.dart';
 import 'package:fulus_mobile/data/repositories/customer_credit_repository_impl.dart';
 import 'package:fulus_mobile/data/repositories/customer_repository_impl.dart';
