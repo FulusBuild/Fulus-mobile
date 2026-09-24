7fd17e915ab972ce7042ca642170715453aa1767
FulusBuild <sannimetaisland@gmail.com>
2026-09-24 08:22:22 +0100

    fix: import sync queue model in category archive test

--- test/sync/category_sync_handler_archive_test.dart
+++ test/sync/category_sync_handler_archive_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
+import 'package:fulus_mobile/data/local/database/database.dart';
 import 'package:fulus_mobile/data/remote/fulus_connection_state.dart';
 import 'package:fulus_mobile/data/remote/fulus_device_registration.dart';
 import 'package:fulus_mobile/data/remote/fulus_sync_api.dart';
