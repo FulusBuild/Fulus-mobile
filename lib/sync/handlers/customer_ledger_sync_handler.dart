2a736f9f9959362256ede3cdd1b1127261e1178a
FulusBuild <sannimetaisland@gmail.com>
2026-09-24 22:47:06 +0100

    fix: initialize shared execution lease in handlers

--- lib/sync/handlers/customer_ledger_sync_handler.dart
+++ lib/sync/handlers/customer_ledger_sync_handler.dart
@@ -21,6 +21,7 @@ class CustomerLedgerSyncHandler implements SyncHandler {
     required SecureStorage secureStorage,
     CustomerRepository? customerRepository,
   })  : _db = db,
+        _executionLease = executionLease,
         _fulusSyncApi = fulusSyncApi,
         _fulusConnectionState = fulusConnectionState,
         _secureStorage = secureStorage,
