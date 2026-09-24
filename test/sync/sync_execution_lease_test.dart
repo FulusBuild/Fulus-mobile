8b6025489ecb11e5b30a0dffd6072265a785dc5a
FulusBuild <sannimetaisland@gmail.com>
2026-09-24 20:48:22 +0100

    fix: import lease fence test dependencies

--- test/sync/sync_execution_lease_test.dart
+++ test/sync/sync_execution_lease_test.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:io';
 
+import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
