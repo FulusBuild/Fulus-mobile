ab610424b6dd9f7a274e8b4c8b78a29ddfb29ec6
FulusBuild <sannimetaisland@gmail.com>
2026-09-25 13:33:28 +0100

    Fix Drift freshness predicates in rejection recovery

--- lib/sync/handlers/sale_sync_handler.dart
+++ lib/sync/handlers/sale_sync_handler.dart
@@ -168,7 +168,7 @@ class SaleSyncHandler implements SyncHandler {
           final newerMovements = await (_db.select(_db.syncQueueItems)
                 ..where((q) => q.entityType.equals('stock_movement'))
                 ..where((q) => q.id.isNotIn([operationId]))
-                ..where((q) => q.enqueuedAt.isBiggerOrEqualValue(enqueuedAt)))
+                ..where((q) => q.enqueuedAt.isBiggerThanValue(enqueuedAt)))
               .get();
           for (final queued in newerMovements) {
             final movement = await (_db.select(_db.stockMovements)
@@ -227,8 +227,7 @@ class SaleSyncHandler implements SyncHandler {
           final ledger = await (_db.select(_db.customerLedgerEntries)
                 ..where((e) => e.localId.equals(queued.entityLocalId))
                 ..where((e) =>
-                    e.customerLocalId.equals(localId) &
-                    e.entryType.equals('repayment')))
+                    e.customerLocalId.equals(localId).and(e.entryType.equals('repayment'))))
               .getSingleOrNull();
           if (ledger != null) return;
         }
