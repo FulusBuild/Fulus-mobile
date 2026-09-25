200d5099301cdb13a55a7a5e620429bb97e8a909
FulusBuild <sannimetaisland@gmail.com>
2026-09-25 07:57:13 +0100

    Add operation identity to sync finalization API

--- lib/domain/repositories/supplier_repository.dart
+++ lib/domain/repositories/supplier_repository.dart
@@ -27,5 +27,5 @@ abstract class SupplierRepository {
   /// Applies a canonical delete without enqueueing an outbound task.
   Future<void> reconcileDeleted(String serverId);
 
-  Future<void> markSynced({required String localId, required String serverId});
+  Future<void> markSynced({required String localId, required String serverId, String? operationId});
 }
