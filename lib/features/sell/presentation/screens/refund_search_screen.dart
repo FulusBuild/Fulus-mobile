56fa40db4899fc7bf9eac8403503cbc4ea5a626f
FulusBuild <sannimetaisland@gmail.com>
2026-09-25 14:42:55 +0100

    fix: handle active location listener states safely

--- lib/features/sell/presentation/screens/refund_search_screen.dart
+++ lib/features/sell/presentation/screens/refund_search_screen.dart
@@ -59,7 +59,7 @@ class _RefundSearchScreenState extends ConsumerState<RefundSearchScreen> {
   @override
   Widget build(BuildContext context) {
     ref.listen(activeLocationIdProvider, (previous, next) {
-      if (previous.value != null && next?.value != null && previous.value != next.value && mounted) {
+      if (previous != null && next != null && previous.value != next.value && mounted) {
         setState(() => _future = _load());
       }
     });
