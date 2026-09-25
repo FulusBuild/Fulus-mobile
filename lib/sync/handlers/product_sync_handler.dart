e3b057bd87cdcbb4c0297e9203d96ce9314e8418
FulusBuild <sannimetaisland@gmail.com>
2026-09-25 08:00:48 +0100

    Use product helper operation identity for sync finalization

--- lib/sync/handlers/product_sync_handler.dart
+++ lib/sync/handlers/product_sync_handler.dart
@@ -30,9 +30,9 @@ class ProductSyncHandler implements SyncHandler {
   Future<void> sync(SyncQueueItem item) async {
     switch (item.operation) {
       case 'create':
-        await _syncCreate(item.entityLocalId, operationId: item.id, baseCursor: item.baseCursor);
+        await _syncCreate(item.entityLocalId, operationId: operationId, baseCursor: item.baseCursor);
       case 'update':
-        await _syncUpdate(item.entityLocalId, operationId: item.id, baseCursor: item.baseCursor);
+        await _syncUpdate(item.entityLocalId, operationId: operationId, baseCursor: item.baseCursor);
       default:
         throw StateError(
           'ProductSyncHandler does not support operation "${item.operation}".',
