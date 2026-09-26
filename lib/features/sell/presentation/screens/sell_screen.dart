d9970e3b68b5ed9aac1b7714d15f32434c1ff070
FulusBuild <sannimetaisland@gmail.com>
2026-09-26 15:37:02 +0100

    fix: simplify sell stock status conditional for codegen

--- lib/features/sell/presentation/screens/sell_screen.dart
+++ lib/features/sell/presentation/screens/sell_screen.dart
@@ -360,9 +360,7 @@ class _ProductRow extends StatelessWidget {
           Text(product.name, maxLines: 2, overflow: TextOverflow.ellipsis, style: AppTypography.caption.copyWith(color: AppColors.textPrimaryOf(context), fontWeight: FontWeight.w700)),
           const SizedBox(height: 2),
           Text(currency + product.sellingPrice.toStringAsFixed(2), style: AppTypography.label.copyWith(color: AppColors.primaryOf(context), fontWeight: FontWeight.w700)),
-          if (out) ...[
-            Text('Out of stock', style: AppTypography.label.copyWith(color: AppColors.errorOf(context))),
-          ],
+          if (out) Text('Out of stock', style: AppTypography.label.copyWith(color: AppColors.errorOf(context))),
         ]),
       ),
     );
