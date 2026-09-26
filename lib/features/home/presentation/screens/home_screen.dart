59df312edf98232e6ec793f07512554e6055e207
FulusBuild <sannimetaisland@gmail.com>
2026-09-26 16:23:18 +0100

    fix: restore home color tokens after cleanup

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -348,4 +348,13 @@ class _HomeHeroSkeleton extends StatelessWidget {
   Widget build(BuildContext context) => const FulusSkeletonBox(height: 170);
 }
 
+class _HomeColors {
+  static const navy = Color(0xFF061B3A);
+  static const blue = Color(0xFF1473E6);
+  static const green = Color(0xFF0BBE6E);
+  static const orange = Color(0xFFFF9F1C);
+  static const purple = Color(0xFF7B3FF2);
+  static const muted = Color(0xFFB7C7DB);
+}
+
 final _businessProfileProvider = StreamProvider.autoDispose<BusinessProfile?>((ref) => ref.watch(businessSettingsRepositoryProvider).watchSettings());
